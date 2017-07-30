bd4c8aae9f7675c3e33d80aa53bad5fe55fd394f
kostyantin2216 <kostyantin2216@hotmail.com>
2017-07-31 00:14:57 +0300

    -

--- src/main/java/com/fixit/components/registration/tradesmen/TradesmanRegistrant.java
+++ src/main/java/com/fixit/components/registration/tradesmen/TradesmanRegistrant.java
@@ -3,6 +3,9 @@
  */
 package com.fixit.components.registration.tradesmen;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.mail.Session;
 import javax.mail.internet.AddressException;
 
@@ -13,9 +16,10 @@ import com.fixit.components.statistics.StatisticsCollector;
 import com.fixit.core.dao.mongo.TradesmanDao;
 import com.fixit.core.dao.sql.StoredPropertyDao;
 import com.fixit.core.dao.sql.TradesmanLeadDao;
+import com.fixit.core.dao.sql.impl.TradesmanLeadDaoImpl;
 import com.fixit.core.data.mongo.Tradesman;
-import com.fixit.core.data.shopify.ShopifyCustomer;
 import com.fixit.core.data.sql.TradesmanLead;
+import com.fixit.core.general.FileManager;
 import com.fixit.core.general.PropertyGroup;
 import com.fixit.core.general.PropertyGroup.Group;
 import com.fixit.core.general.StoredProperties;
@@ -30,6 +34,7 @@ import com.fixit.core.utils.Constants;
 @Component
 public class TradesmanRegistrant {
 	
+	private final FileManager mFileManager;
 	private final TradesmanDao mTradesmanDao;
 	private final TradesmanLeadDao mLeadDao;
 	private final StoredPropertyDao mPropertyDao;
@@ -37,9 +42,10 @@ public class TradesmanRegistrant {
 	private final SimpleEmailSender mMailSender;
 	
 	@Autowired
-	public TradesmanRegistrant(TradesmanDao tradesmanDao, TradesmanLeadDao tradesmanLeadDao, 
+	public TradesmanRegistrant(FileManager fileManager, TradesmanDao tradesmanDao, TradesmanLeadDao tradesmanLeadDao, 
 							   StoredPropertyDao storedPropertyDao, StatisticsCollector statisticsCollector, 
 							   Session session) {
+		mFileManager = fileManager;
 		mTradesmanDao = tradesmanDao;
 		mLeadDao = tradesmanLeadDao;
 		mPropertyDao = storedPropertyDao;
@@ -54,39 +60,46 @@ public class TradesmanRegistrant {
 		}
 	}
 	
-	public void newRegistration(ShopifyCustomer shopifyCustomer) {
-		TradesmanLead lead = TradesmanLead.newLead(
-				shopifyCustomer.getId(),
-				shopifyCustomer.getFirst_name(),
-				shopifyCustomer.getLast_name(),
-				shopifyCustomer.getEmail()
-		);
-		
+	public void newLead(TradesmanLead lead) {
 		if(mLeadDao.isNewLead(lead)) {
 			registerLead(lead);
 		} else {
+			TradesmanLead registeredLead = mLeadDao.findOneByProperty(TradesmanLeadDaoImpl.PROP_EMAIL, lead.getEmail());
+			if(!registeredLead.isEmailSent()) {
+				sendEmail(lead);
+				mLeadDao.update(lead);
+			}
 			FILog.w(Constants.LT_TRADESMAN_REGISTRATION, "Couldn't store lead: " + lead, true);
 		}
 	}
 	
-	private void registerLead(TradesmanLead lead) {
+	private void sendEmail(TradesmanLead lead) {
 		String subject = "Fix It Registration";
 		String content = "Hello " + lead.getFirstName() + " " + lead.getLastName() + "\n"
-						+ "please complete your registration here http://www.fixxit.com/web/registration/" + lead.getId(); 
+						+ "please complete your registration here http://fixxit-web.co.za/web/tradesmanRegistration?leadId=" + lead.getId(); 
 
 		boolean sentSuccessfully = mMailSender.sendMail(subject, content, lead.getEmail());
 		lead.setEmailSent(sentSuccessfully);
-		
+	}
+	
+	private void registerLead(TradesmanLead lead) {
 		mLeadDao.save(lead);
+		
+		sendEmail(lead);
 	}
 	
 	public TradesmanLead findLead(long leadId) {
 		return mLeadDao.findById(leadId);
 	}
 	
-	public void registerTradesman(Tradesman tradesman) {
+	public void registerTradesman(long leadId, Tradesman tradesman, InputStream logoInputStream, String logoFileExtension, InputStream featureInputStream, String featureFileExtension) throws IOException {
 		mTradesmanDao.save(tradesman);
+		String tradesmanId = tradesman.get_id().toHexString();
+		mFileManager.storeTradesmanLogo(tradesmanId, logoFileExtension, logoInputStream);
+		mFileManager.storeTradesmanFeatureImage(tradesmanId, featureFileExtension, featureInputStream);
 		mStatsCollector.tradesmanRegistered(tradesman);
+		TradesmanLead lead = findLead(leadId);
+	//	lead.setTradesmanId(tradesmanId);
 	}
 	
 }
